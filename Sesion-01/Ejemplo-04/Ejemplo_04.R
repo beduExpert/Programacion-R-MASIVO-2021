450670b628870dc865839915f6e879c821636b66
Erick <e.coronadoj@gmail.com>
2021-05-12 12:50:48 -0500

    Update Ejemplo_04.R

--- Sesion-01/Ejemplo-04/Ejemplo_04.R
+++ Sesion-01/Ejemplo-04/Ejemplo_04.R
@@ -21,7 +21,7 @@ getwd()
 
 # Fijando el directorio de trabajo
 
-setwd("c:/Users/User/Documents/Bedu/") # Depende del usuario
+setwd("c:/DATA/Sesion1/") # Depende del usuario donde ubicar su wd
 
 # La función read.csv será util para leer fichero .csv
 
