7fce81b8b2fdff2a4fd75213b699df3c078731b4
Erick <e.coronadoj@gmail.com>
2021-05-18 22:45:34 -0500

    Update Ejemplo_05.R

--- Sesion-01/Ejemplo-05/Ejemplo_05.R
+++ Sesion-01/Ejemplo-05/Ejemplo_05.R
@@ -23,9 +23,16 @@ library(ggplot2) # Se omite el uso de las comillas
 
 install.packages("dplyr")
 library(dplyr)
+# También se puede realizar esta acción en la pestaña de Packages, intentalo.
 
 # Algo útil es revisar la versión que se tiene de R, ya que en ocasiones no se instalan ciertos paquetes por la incompatibilidad de R, el siguiente comando nos indicará la versión que estamos trabajando.
 
 version
 
-# También se puede realizar esta acción en la pestaña de Packages, intentalo.
+#En caso de que tu versión de R no sea la más reciente puedes intentar desde el RGui.exe lo siguiente para actualizarlo a la última versión (recuerda que no se hace desde RSTudio)
+
+
+install.packages("installr", dependencies = TRUE)
+library(installr)
+updateR()
+
