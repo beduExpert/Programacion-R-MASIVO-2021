d6801735d6b87a71f152aa7f2948b0338c2971a5
Erick <e.coronadoj@gmail.com>
2021-05-18 23:59:48 -0500

    Update Ejemplo 2-3.R

--- Sesion-02/Ejemplo-03/Ejemplo 2-3.R
+++ Sesion-02/Ejemplo-03/Ejemplo 2-3.R
@@ -1,82 +1,49 @@
-# 2.3 Paquete dplyr y aplicaciones
+# Ejemplo 3. Funciones `na.omit` y `complete.cases`
 
-# El paquete dplyr cuenta con varias funciones muy útiles, para manipular 
-# y transformar data frames. Una vez instalado el paquete dplyr puede cargarlo
-# en R de la siguiente manera (Sin mensajes ni advertencias)
+# Ahora vamos a considerar el conjunto de datos `airquality`, observamos primero
+# algunas de sus filas
 
-suppressMessages(suppressWarnings(library(dplyr)))
+head(airquality)
+library(dplyr)
 
-# Vamos a descargar archivos csv que contienen datos del covid-19 para mostrar
-# como funcionan algunas funciones del paquete dplr. Las url desde las
-# cuales descargamos los datos son las siguientes
+# El tipo de objeto que es y el tipo de variables que contiene
 
-url1 <- "https://data.humdata.org/hxlproxy/data/download/time_series_covid19_confirmed_global_narrow.csv?dest=data_edit&filter01=explode&explode-header-att01=date&explode-value-att01=value&filter02=rename&rename-oldtag02=%23affected%2Bdate&rename-newtag02=%23date&rename-header02=Date&filter03=rename&rename-oldtag03=%23affected%2Bvalue&rename-newtag03=%23affected%2Binfected%2Bvalue%2Bnum&rename-header03=Value&filter04=clean&clean-date-tags04=%23date&filter05=sort&sort-tags05=%23date&sort-reverse05=on&filter06=sort&sort-tags06=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv"
-url2 <- "https://data.humdata.org/hxlproxy/data/download/time_series_covid19_deaths_global_narrow.csv?dest=data_edit&filter01=explode&explode-header-att01=date&explode-value-att01=value&filter02=rename&rename-oldtag02=%23affected%2Bdate&rename-newtag02=%23date&rename-header02=Date&filter03=rename&rename-oldtag03=%23affected%2Bvalue&rename-newtag03=%23affected%2Binfected%2Bvalue%2Bnum&rename-header03=Value&filter04=clean&clean-date-tags04=%23date&filter05=sort&sort-tags05=%23date&sort-reverse05=on&filter06=sort&sort-tags06=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv"
+str(airquality)
 
-# Descargamos los datos en nuestro directorio de trabajo con la siguiente instrucción
+# observamos la dimensión
 
-download.file(url = url1, destfile = "st19ncov-confirmados.csv", mode = "wb")
-download.file(url = url2, destfile = "st19ncov-muertes.csv", mode = "wb")
+dim(airquality)
 
-# Una vez que hemos descargado los datos importamos los datos de casos
-# confirmados y muertes a R
+# Con la función `complete.cases` podemos averiguar cuales son aquellas filas
+# que no contienen ningún valor perdido (NA) y cuales son aquellas filas
+# que tienen al menos un valor perdido.
 
-conf <- read.csv("st19ncov-confirmados.csv") #Casos confirmados
-dec <- read.csv("st19ncov-muertes.csv")     # Decesos confirmados
+bien <- complete.cases(airquality)
 
-str(conf); str(dec)
-head(conf); head(dec)
+# La variable bien, es un vector lógico con `TRUE` en las posiciones que 
+# representan filas de `airquality` en donde no hay NA's y con `FALSE`
+# en las posiciones que representan aquellas filas de `airquality` en donde
+# se encontraron NA's
 
-# Ahora seleccionamos todas las filas menos la primera, esto para cada
-# data frame
+# Por tanto, podemos contar el número de filas en donde no hay NA´s de la 
+# siguiente manera
 
-Sconf <- conf[-1, ]
-Sdec <- dec[-1, ]
+sum(bien)
 
-# Con la función select del paquete dplyr, del data frame de casos confirmados
-# seleccionamos únicamente las columnas de País, Fecha, Número acumulado de casos
+# Podemos filtrar aquellas filas sin NA's de la siguiente manera
 
-Sconf <- select(Sconf, Country.Region, Date, Value) # País, fecha y acumulado de infectados
+airquality[bien,]
 
-# Con la función rename, renombramos las columnas correspondientes al país
-# y al número acumulado de casos de infectados por covid-19
+data <- select(airquality, Ozone:Temp)
+apply(data, 2, mean)
+apply(data, 2, mean, na.rm = T)
 
-Sconf <- rename(Sconf, Country = Country.Region, Fecha = Date, Infectados = Value)
+# `na.omit` devuelve el objeto con casos incompletos eliminados
 
-str(Sconf)
+(m1 <- apply(na.omit(data), 2, mean))
 
-# Como cada una de las columnas del último data frame aparecen como factor,
-# con la función mutate transformamos las columnas correspondientes a fechas
-# y a número de infectados, esto para que R reconozca como fechas la 
-# columna correspondiente y como números los elementos de la columna que 
-# indica el acumulado de casos.
+b <- complete.cases(data)
 
-Sconf <- mutate(Sconf, Fecha = as.Date(Fecha, "%Y-%m-%d"), Infectados = as.numeric(Infectados))
+(m2 <- apply(data[b,], 2, mean))
 
-# Hacemos algo similar con el data frame correspondiente al número
-# acumulado de decesos
-
-Sdec <- select(Sdec, Country.Region, Date, Value) # Seleccionamos país, fecha y acumulado de decesos
-Sdec <- rename(Sdec, Country = Country.Region, Fecha = Date, Decesos = Value) # Renombramos
-Sdec <- mutate(Sdec, Fecha = as.Date(Fecha, "%Y-%m-%d"), Decesos = as.numeric(Decesos)) 
-
-
-Scm <- merge(Sconf, Sdec) # Unimos infectados y decesos acumulados para cada fecha
-dim(Scm)
-
-mex <- filter(Scm, Country == "Mexico") # Seleccionamos sólo a México
-mex <- filter(mex, Infectados != 0) # Primer día de infectados
-
-mex <- mutate(mex, NI = c(1, diff(Infectados))) # Nuevos infectados por día
-mex <- mutate(mex, ND = c(0, diff(Decesos))) # Nuevos decesos por día
-
-mex <- mutate(mex, Letalidad = round(Decesos/Infectados*100, 1)) # Tasa de letalidad
-
-mex <- mutate(mex, IDA = lag(Infectados), DDA = lag(Decesos)) # Valores día anterior
-mex <- mutate(mex, FCI = Infectados/IDA, FCD = Decesos/DDA) # Factores de Crecimiento
-mex <- mutate(mex, Dia = 1:dim(mex)[1]) # Días de contingencia
-
-head(mex); tail(mex)
-write.csv(mex, "C19Mexico.csv", row.names = FALSE)
-
-# Nota: en el ejemplo 5 de la sesión 3 se hará uso del archivo creado C19Mexico.csv
+identical(m1, m2)
