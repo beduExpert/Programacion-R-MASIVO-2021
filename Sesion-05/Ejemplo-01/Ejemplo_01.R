a6f8ec00895bd659e8e6250a820ce2d47a16d88c
Erick <e.coronadoj@gmail.com>
2021-06-04 11:25:26 -0500

    Update Ejemplo_01.R

--- Sesion-05/Ejemplo-01/Ejemplo_01.R
+++ Sesion-05/Ejemplo-01/Ejemplo_01.R
@@ -1,142 +1,155 @@
-# Ejemplo 1. Regresión Lineal Simple
+# Ejemplo 1. Regresión Lineal Múltiple
 
-# Primero hay que establecer el directorio de trabajo y este deberá contener 
-# el archivo de datos production.txt
+# Predecir el precio de cena (platillo). 
+# Datos de encuestas de clientes de 168 restaurantes Italianos
+# en el área deseada están disponibles.
 
-# Leemos nuestros datos con la función read.table
+# Y: Price (Precio): el precio (en USD) de la cena
+# X1: Food: Valuación del cliente de la comida (sacado de 30)
+# X2: Décor: Valuación del cliente de la decoración (sacado de 30)
+# X3: Service: Valuación del cliente del servicio (sacado de 30)
+# X4: East: variable dummy: 1 (0) si el restaurante está al este (oeste) de la quinta avenida
 
-production <- read.table("production.txt", header = TRUE)
+# Primero debemos establecer nuestro directorio de trabajo y el archivo
+# de datos (nyc.csv) que importaremos a R deberá de estar en este directorio
 
-# Los datos que importamos a R se encuentran como data frame con nombre 
-# production. Aplicamos la función attach al data frame production para
-# poder manipular las columnas mediante sus nombres
+nyc <- read.csv("nyc.csv", header = TRUE)
 
-attach(production)
+# Observamos algunas filas y la dimensión del data frame
 
-# Hacemos el gráfico de dispersión
+tail(nyc, 2) 
+dim(nyc)
+attach(nyc)
 
-plot(RunSize, RunTime, xlab = "Tamaño de ejecución", 
-     ylab = "Tiempo de ejecución", pch = 16)
+# Acontinuación mostramos una matriz de gráficos de dispersión de los
+# tres predictores continuos y la variable de respuesta. 
 
-# Ajustamos un modelo de regresión lineal simple con la función lm, en donde
-# la variable de respuesta es RunTime y la variable predictora es RunSize. 
-# Guardamos nuestro modelo ajustado con el nombre de m1
+pairs(~ Price + Food + Decor + Service, data = nyc, gap = 0.4, cex.labels = 1.5)
 
-m1 <- lm(RunTime~RunSize)
+# Observamos relaciones aproximadamente lineales
 
-# Obtenemos un resumen de nuestro modelo ajustado mediante la función `summary`
+# Llevamos a cabo el ajuste de un modelo
+# Y = beta0 + beta1*Food + beta2*Decor + beta3*Service + beta4*East + e
+
+m1 <- lm(Price ~ Food + Decor + Service + East)
+
+# Obtenemos un resumen
 
 summary(m1)
 
-# Graficamos nuestros datos nuevamente, pero ahora con la recta de regresión
-# ajustada
+# Ajustamos nuevamente un modelo pero ahora sin considerar la variable Service
+# ya que en el resultado anterior se observó que su coeficiente de regresión
+# no fue estadísticamente significativo (al considerar su p-value)
 
-plot(RunSize, RunTime, xlab = "Tamaño de ejecución", 
-     ylab = "Tiempo de ejecución", pch = 16)
-abline(lsfit(RunSize, RunTime)) # Trazamos la recta de regresión estimada
-mtext(expression(paste('Modelo de regresión lineal simple:',
-                       ' ',
-                       y[i] == beta[0] + beta[1]*x[i] + e[i])),
-      side = 3, adj=1, font = 2)
+# Y = beta0 + beta1*Food + beta2*Decor + beta4*East + e (Reducido)
 
-# Recta de regresión poblacional
+m2 <- lm(Price ~ Food + Decor + East)
 
-text(x = 200, y = 240, expression(paste('Recta de regresión:',
-                                        ' ',
-                                        y[i] == beta[0] + beta[1]*x[i])),
-     adj = 1, font = 2)
+# Obtenemos un resumen del modelo ajustado
+
+summary(m2)
+
+# Una forma alternativa de obtener m2 es usar el comando update
+
+m2 <- update(m1, ~.-Service)
+summary(m2)
+
+######
+
+# Análisis de covarianza
+
+# Para investigar si el efecto de los predictores depende de la variable dummy 
+# East consideraremos el siguiente modelo el cual es una extensión a más de una 
+# variable predictora del modelo de rectas de regresión no relacionadas 
+# Y = beta0 + beta1*Food + beta2*Decor +  beta3*Service + beta4*East 
+#           + beta5*Food*East + beta6*Decor*East + beta7*Service*East + e (Completo)
+
+mfull <- lm(Price ~ Food + Decor + Service + East + 
+              Food:East + Decor:East + Service:East)
+
+# Note como ninguno de los coeficientes de regresión para los
+# términos de interacción son estadísticamente significativos
+
+summary(mfull)
+
+# Ahora compararemos el modelo completo guardado en mfull contra el modelo
+# reducido guardado en m2. Es decir, llevaremos a cabo una prueba de hipótesis
+# general de
+
+# H0: beta3 = beta5 = beta6 = beta7 = 0
+# es decir Y = beta0 + beta1*Food + beta2*Decor + beta4*East + e (Reducido)
+# contra
+# H1: H0 no es verdad
+# es decir, 
+# Y = beta0 + beta1*Food + beta2*Decor +  beta3*Service + beta4*East 
+#           + beta5*Food*East + beta6*Decor*East + beta7*Service*East + e (Completo)
+
+# La prueba de si el efecto de los predictores depende de la variable dummy
+# East puede lograrse usando la siguiente prueba-F parcial.
+
+anova(m2,mfull)
+
+# Dado que el p-value es aproximadamente 0.36, fallamos en rechazar la hipótesis
+# nula y adopatamos el modelo reducido
+# Y = beta0 + beta1*Food + beta2*Decor + beta4*East + e (Reducido)
+
+######
+
+# Diagnósticos
+
+# En regresión múltiple, las gráficas de residuales o de residuales
+# estandarizados proporcionan información directa sobre la forma
+# en la cual el modelo está mal especificado cuando se cumplen
+# las siguientes dos condiciones:
+
+# E(Y | X = x) = g(beta0 + beta1*x1 + ... + betap*xp) y
+# E(Xi | Xj) aprox alpha0 + alpha1*Xj
+
+# Cuando estas condiciones se cumplen, la gráfica de Y contra
+# los valores ajustados, proporciona información directa acerca de g.
+# En regresión lineal múltiple g es la función identidad. En
+# este caso la gráfica de Y contra los valores ajustados
+# debe producir puntos dispersos alrededor de una recta.
+# Si las condiciones no se cumplen, entonces un patrón en la
+# gráfica de los residuales indica que un modelo incorrecto
+# ha sido ajustado, pero el patrón mismo no proporciona 
+# información directa sobre como el modelo está mal específicado.
+
+# Ahora tratemos de verificar si el modelo ajustado es un modelo válido.
+
+summary(m2)
+
+# Mostramos una gráfica de Y, el precio contra los valores
+# ajustados 
+
+plot(m2$fitted.values, Price, xlab = "Valores ajustados", ylab = "Price")
+abline(lsfit(m2$fitted.values, Price))
+
+# Acontinuación mostramos una matriz de gráficos de dispersión de los
+# dos predictores continuos. Los predictores parecen estar linealmente
+# relacionados al menos aproximadamente
+
+pairs(~ Food + Decor, data = nyc, gap = 0.4, cex.labels = 1.5)
 
 
-# Recta de regresión estimada
+# Acontinuación veremos gráficas de residuales estandarizados contra cada
+# predictor. La naturaleza aleatoria de estas gráficas es un indicativo de
+# que el modelo ajustado es un modelo válido para los datos.
 
-text(x = 350, y = 180, expression(paste('Recta estimada:',
-                                        ' ',
-                                        hat(y)[i] == hat(beta)[0] + hat(beta)[1]*x[i])),
-     adj = 1, font = 2)
-
-# Recta de regresión estimada
-
-text(x = 350, y = 160, expression(paste('Recta estimada:',
-                                        ' ',
-                                        hat(y)[i] == 149.74770 + 0.25924*x[i])),
-     adj = 1, font = 2)
-
-# Residuales
-
-points(189, 215, pch=16, col = "red") # Punto muestral
-149.74770 + 0.25924 * 189 # Valor y sobre la recta estimada
-lines(c(189, 189), c(198.7441, 215), col = "red")
-
-points(173, 166, pch=16, col = "red") # Punto muestral
-149.74770 + 0.25924 * 173 # Valor y sobre la recta estimada
-lines(c(173, 173), c(166, 194.5962), col = "red")
-
-# Acontinuación encontramos el cuantil de orden 0.975 de la distribución
-# t de Student con 18 (n - 2) grados de libertad. En total tenemos n = 20 
-# observaciones en nuestro conjunto de datos. Estamos encontrando el valor 
-# que satisface P(T > tval) = 0.025
-
-tval <- qt(1-0.05/2, 18)
-tval
-
-# Comprobamos
-
-pt(tval, df = 18)
-
-# Encontramos intervalos de confianza del 95% para el intercepto y la pendiente
-# del modelo de regresión lineal simple
-
-round(confint(m1, level = 0.95), 3)
-
-# Ahora encontramos intervalos de confianza del 95% para la recta de regresión
-# poblacional en algunos valores de X (RunSize)
-
-RunSize0 <- c(50,100,150,200,250,300,350) # Algunos posibles valores de RunSize
-
-(conf <- predict(m1, newdata = 
-                   data.frame(RunSize = RunSize0), 
-                 interval = "confidence", level = 0.95))
-
-# Podemos visualizar gráficamente estos intervalos de confianza
-
-lines(RunSize0, conf[, 2], lty = 2, lwd = 2, col = "green") # límites inferiores
-lines(RunSize0, conf[, 3], lty = 2, lwd = 2, col = "green") # límites superiores
-
-# También podemos encontrar intervalos de predicción del 95% para el valor
-# real de la variable de respuesta Y (RunTime) en algunos valores de X (RunSize)
-
-(pred <- predict(m1, newdata = 
-          data.frame(RunSize = RunSize0), 
-        interval = "prediction", level = 0.95))
-
-# Podemos visualizar gráficamente estos intervalos de predicción
-
-lines(RunSize0, pred[, 2], lty = 2, lwd = 2, col = "blue") # límites inferiores
-lines(RunSize0, pred[, 3], lty = 2, lwd = 2, col = "blue") # límites superiores
-
-
-# Note como los intervalos de confianza están contenidos dentro de los
-# intervalos de predicción correspondientes
-
-# También es posible llevar a cabo un análisis de varianza para decidir si 
-# existe asociación lineal entre RunSize y RunTime
-
-anova(m1)
-
-# Gráfico de diagnóstico de R
-
-# Cuando usamos un modelo de regresión, hacemos una serie de suposiciones. 
-# Entonces debemos hacer diagnósticos de regresión para verificar las
-# supocisiones.
-
+StanRes2 <- rstandard(m2)
 par(mfrow = c(2, 2))
-plot(m1)
+plot(Food, StanRes2, ylab = "Residuales Estandarizados")
+plot(Decor, StanRes2, ylab = "Residuales Estandarizados")
+plot(East, StanRes2, ylab = "Residuales Estandarizados")
+
+# Buscamos evidencia para soportar la supocisión de normalidad en los errores 
+
+qqnorm(StanRes2)
+qqline(StanRes2)
+
 dev.off()
 
 # Inspirado en:
 
 # [S.J. Sheather, A Modern Approach to Regression with R, DOI: 10.1007/978-0-387-09608-7_2, © Springer Science + Business Media LLC 2009](https://gattonweb.uky.edu/sheather/book/index.php)
-
-
-
 
