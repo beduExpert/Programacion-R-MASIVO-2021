6b0aaf4f79cd3c959ece0856e2b205d88f95297d
Erick <e.coronadoj@gmail.com>
2021-06-08 17:29:48 -0500

    Update Ejemplo 2-2.R

--- Sesion-02/Ejemplo-02/Ejemplo 2-2.R
+++ Sesion-02/Ejemplo-02/Ejemplo 2-2.R
@@ -1,26 +1,8 @@
 # 2.1 Características de los objetos (str sobre df, summary, head y view) y funciones
 
-# Función str
-
-# str es una función que muestra de manera compacta la estructura interna 
-# de un objeto de R. Por ejemplo, si usamos como argumento de str el 
-# conjunto de datos iris que podemos encontrar en R
-
-str(iris)
-
-# entonces la salida de la instrucción nos muestra el tipo de objeto, 
-# número de observaciones y de variables, así como el tipo de dato al 
-# que corresponde cada variable.
-
 # Función summary
 
-# La función summary es una función genérica usada para obtener resumenes 
-# de diferentes objetos de R, por ejemplo
-
-summary(1:100)
-summary(mtcars)
-
-# También es útil para obtener resumenes de los resultados de diferentes
+# La función summary es una función genérica usada para obtener resumenes sin embargo, también es útil para obtener resumenes de los resultados de diferentes
 # ajustes a modelos
 
 set.seed(57)
@@ -29,28 +11,6 @@ e <- rnorm(35)
 y <- 5 + 2*x + e
 modelo <- lm(y~x)
 summary(modelo)
-
-# Función head
-
-# La función head devuelve la primera parte de un data frame, tabla, 
-# matriz, vector o función. Por ejemplo, al usar el data frame mtcars
-# como argumento de la función head, se devolverán únicamente las
-# primeras seis filas del data frame
-
-head(mtcars)
-
-# la función tail funciona de manera similar, pero en lugar de devolver
-# la primera parte de un objeto, devuelve la última parte de este,
-# por ejemplo, al ejecutarse la siguiente instrucción
-
-tail(mtcars)
-
-# se devolverán las últimas seis filas del data frame
-
-# Función view
-
-# La función View aplicada a un objeto de R como un data frame, 
-# invoca un visor de datos al estilo de una hoja de cálculo, por ejemplo
 
 View(iris)
 
