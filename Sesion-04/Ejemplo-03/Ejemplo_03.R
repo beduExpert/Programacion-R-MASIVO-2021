ff94116ec8e13a9dbee2519bd42895241e2ed479
Erick <e.coronadoj@gmail.com>
2021-06-15 22:35:02 -0500

    Update Ejemplo_03.R

--- Sesion-04/Ejemplo-03/Ejemplo_03.R
+++ Sesion-04/Ejemplo-03/Ejemplo_03.R
@@ -1,114 +1,81 @@
-# Ejemplo 3. Algunos estimadores puntuales insesgados comunes
+# EJEMPLO 3. Contraste de hipótesis
 
-# Nota: este ejemplo es opcional por su similaridad con el ejemplo sobre el teorema central del límite
+# Contrastes comunes con muestras grandes
 
-# Cargamos el paquete ggplot2 para hacer algunas gráficas
+# Contraste de dos colas
+# Dada dos muestras aleatorias de tamaños n1 = 56 y n2 = 63
 
-library(ggplot2)
-# Consideremos una variable aleatoria (v.a.) X con distribución exponencial y parámetro lambda = 3
+set.seed(174376)
+m1 <- rexp(n = 56, rate = 4.1); 1/4.1 # media real de la población
+tail(as.data.frame(m1))
+m2 <- rexp(n = 63, rate = 3.4); 1/3.4 # media real de la población
+tail(as.data.frame(m2))
+1/4.1-1/3.4 # diferencia de medias real
+# estamos interesados en contrastar las hipótesis H0: mu1-mu2 = 0 vs H1: mu1-mu2 diferente de 0 (contraste de dos colas)
 
-x <- seq(0, 5, 0.02)
-plot(x, dexp(x, rate = 3), type = "l", lwd = 2, ylab = "")
-title(main = "Función de Densidad Exponencial", ylab = "f(x)",
-      sub = expression("Parámetro " ~ lambda == 3))
-text(x = 3, y = 1.5, labels = expression(f(x)==3*exp(-3*x) ~ " para x "  >= 0))
-text(x = 3, y = 1.3, labels = paste("0 en otro caso"))
-text(x = 1, y = 1, labels = expression("E(X) = " ~ 1/lambda == 1/3), col = 2)
-text(x = 3, y = 0.5, labels = expression("DE(X) = " ~ 1/lambda == 1/3), col = 4)
-# Obtenemos 1200 muestras aleatorias de tamaño 350 y las 1200 medias correspondientes a las muestras
+# El valor observado del estadístico de prueba en este caso está dado por
 
-set.seed(65) # Para reproducir las muestras en el futuro
-m1200.350 <- sapply(X = rep(350, 1200), FUN = rexp, rate = 3)
-media1200.350 <- apply(m1200.350, 2, mean)
-mdf <- as.data.frame(media1200.350)
-tail(mdf)
-# Observamos que el histograma de las medias tiene forma de campana
+z0 <- (mean(m1)-mean(m2)-0)/sqrt(var(m1)/56 + var(m2)/63)
+z0
+# que proviene de una distribución normal estándar aproximadamente.
 
-ggplot(mdf, aes(media1200.350)) + 
-  geom_histogram(colour = 'yellow', 
-                 fill = 'purple',
-                 alpha = 0.7) + # Intensidad del color fill
-  geom_vline(xintercept = mean(media1200.350), linetype="dashed", color = "black") + 
-  ggtitle('Histograma para las 1200 medias') + 
-  labs(x = 'medias', y = 'Frecuencia')+
-  theme_get() +
-  theme(plot.title = element_text(hjust = 0.5, size = 16)) 
-mean(media1200.350); 1/3 # Media de las 1200 medias y media de la población de la cual provienen las 1200 muestras
-sd(media1200.350); (1/3)/sqrt(350) # DE de las 1200 medias y DE de la población de la cual provienen las 1200 muestras dividida por la raíz del tamaño de las muestras
+# Supongamos que estamos interesados en encontrar la región de rechazo (de dos colas) con un nivel de significancia alpha = 0.05, debemos encontrar el valor z_{0.025} que satisface P(Z > z_{0.025}) = 0.025.
 
-# Ensayo Bernoulli. Con las siguientes instrucciones obtenemos un solo valor, en donde el 0 (fracaso) puede ocurrir con probabilidad 0.45 y el 1 (éxito) con probabilidad p = 0.55
+(z.025 <- qnorm(p = 0.025, lower.tail = FALSE))
+# Como
 
-set.seed(345)
-sample(x = c(0, 1), size = 1, prob = c(0.45, 0.55))
-rbinom(n = 1, size = 1, prob = 0.55)
-# Obtenemos 1000 muestras de tamaño 31 de una v.a. Bernoulli con p = 0.55
+(z0 < -z.025) | (z0 > z.025)
+# fallamos en rechazar la hipótesis nula.
 
-set.seed(5434) # Para reproducir las muestras en el futuro
-m1000.31 <- sapply(X = rep(31, 1000), FUN = function(n) sample(x = c(0, 1), size = n, replace = TRUE, prob = c(0.45, 0.55)))
-media1000.31 <- apply(m1000.31, 2, mean)
-mdf <- as.data.frame(media1000.31)
-tail(mdf)
-# Observamos que el histograma de las medias es parecida a una campana
+# p-value El p-value lo podemos calcular como
 
-ggplot(mdf, aes(media1000.31)) + 
-  geom_histogram(colour = 'orange', 
-                 fill = 'blue',
-                 alpha = 0.7) + # Intensidad del color fill
-  geom_vline(xintercept = mean(media1000.31), linetype="dashed", color = "black") + 
-  ggtitle('Histograma para las 1000 medias') + 
-  labs(x = 'medias', y = 'Frecuencia')+
-  theme_grey() +
-  theme(plot.title = element_text(hjust = 0.5, size = 16)) 
-mean(media1000.31); 0.55 # Media de las 1000 medias y media de la población de la cual provienen las 1000 muestras
-sd(media1000.31); sqrt(0.55*0.45)/sqrt(31) # DE de las 1000 medias y DE de la población de la cual provienen las 1000 muestras dividida por la raíz del tamaño de la muestra
+(pvalue <- 2*pnorm(z0, lower.tail = FALSE))
+x <- seq(-4, 4, 0.01)
+y <- dnorm(x)
+plot(x, y, type = "l", xlab="", ylab="")
+title(main = "Densidad normal estándar", sub = expression(paste(mu == 0, " y ", sigma == 1)))
 
+polygon(c(min(x), x[x<=-z0], -z0), c(0, y[x<=-z0], 0), col="purple")
+axis(side = 1, at = -z0, font = 2, padj = 1, lwd = 2)
 
-# Obtenemos 1150 muestras aleatorias de tamaño n1 = 54 de una distribución exponencial con parámetro lambda1 = 3.2 y otras 1150 muestras aleatorias de tamaño n2 = 41 de una distribución exponencial con parámetro lambda2 = 1.5.
+polygon(c(z0, x[x>=z0], max(x)), c(0, y[x>=z0], 0), col="purple")
+axis(side = 1, at = z0, font = 2, padj = 1, lwd = 2)
 
-set.seed(65) # Para reproducir las muestras en el futuro
-m1150.54 <- sapply(X = rep(54, 1150), FUN = rexp, rate = 3.2)
-media1150.54 <- apply(m1150.54, 2, mean)
-m1150.41 <- sapply(X = rep(41, 1150), FUN = rexp, rate = 1.5)
-media1150.41 <- apply(m1150.41, 2, mean)
-dif.medias <- media1150.54 - media1150.41 # Diferencia de medias
-dmdf <- as.data.frame(dif.medias)
-tail(dmdf)
-# Observamos que el histograma de las diferencias de medias es parecida a una campana
+# Contraste de hipótesis con muestras pequeñas para mu y mu1 - mu2
 
-ggplot(dmdf, aes(dif.medias)) + 
-  geom_histogram(colour = 'orange', 
-                 fill = 'red',
-                 alpha = 0.7) + # Intensidad del color fill
-  geom_vline(xintercept = mean(dif.medias), linetype="dashed", color = "black") + 
-  ggtitle('Histograma para las 1000 diferencias de medias') + 
-  labs(x = 'medias', y = 'Frecuencia')+
-  theme_grey() +
-  theme(plot.title = element_text(hjust = 0.5, size = 16)) 
-mean(dif.medias); 1/3.2-1/1.5 # Media de las 1150 diferencias de medias y diferencia de medias de las poblaciones de las cuales provienen las 11500 muestras
+# Contraste de dos colas
+# Dada dos muestras aleatorias de tamaños n1 = 23 y n2 = 20
 
-sd(dif.medias); sqrt((1/3.2^2)/54 + (1/1.5^2)/41) # DE de las 1150 diferencias de medias y DE dada en literatura
+set.seed(1776)
+m1 <- rnorm(n = 23, mean = 175, sd = 3)
+tail(as.data.frame(m1))
+m2 <- rnorm(n = 20, mean = 160, sd = 3)
+tail(as.data.frame(m2))
+175-160 # diferencia de medias real
+# estamos interesados en contrastar las hipótesis H0: mu1-mu2 = 0 vs H1: mu1-mu2 diferente de 0 (contraste de dos colas)
 
-# Obtenemos 1100 muestras de tamaño n1 = 48 de una v.a. Bernoulli con p1 = 0.65 y otras 1100 muestras de tamaño n2 = 35 de una v.a. Bernoulli con p2 = 0.53
+# El valor observado del estadístico de prueba en este caso está dado por
 
-set.seed(7434) # Para reproducir las muestras en el futuro
-m1100.48 <- sapply(X = rep(48, 1100), FUN = function(n) sample(x = c(0, 1), size = n, replace = TRUE, prob = c(0.35, 0.65)))
-m1100.35 <- sapply(X = rep(35, 1100), FUN = function(n) sample(x = c(0, 1), size = n, replace = TRUE, prob = c(0.47, 0.53)))
-media1100.48 <- apply(m1100.48, 2, mean)
-media1100.35 <- apply(m1100.35, 2, mean)
-dif.medias <- media1100.48 - media1100.35
-dmdf <- as.data.frame(dif.medias)
-tail(dmdf)
-# Observamos que el histograma de las diferencias de medias es parecida a una campana
+t0 <- (mean(m1)-mean(m2)-0)/(sqrt((22*var(m1)+19*var(m2))/(23+20-2))*sqrt(1/23+1/20))
+t0
+# que proviene de una distribución t de Student con 23 + 20 - 2 = 41 gl
 
-ggplot(dmdf, aes(dif.medias)) + 
-  geom_histogram(colour = 'pink', 
-                 fill = 'blue',
-                 alpha = 0.7) + # Intensidad del color fill
-  geom_vline(xintercept = mean(dif.medias), linetype="dashed", color = "black") + 
-  ggtitle('Histograma para las 1100 diferencias de medias') + 
-  labs(x = 'medias', y = 'Frecuencia')+
-  theme_grey() +
-  theme(plot.title = element_text(hjust = 0.5, size = 16)) 
-mean(dif.medias); 0.65 - 0.53 # Media de las 1100 diferencias de medias y diferencia de medias de las poblaciones de las cuales provienen las muestras
-sd(dif.medias); sqrt((0.65*0.35)/48 + (0.53*0.47)/35) # DE de las 1100 diferencias de medias y DE dada en literatura
+# Supongamos que estamos interesados en encontrar la región de rechazo (de dos colas) con un nivel de significancia alpha = 0.05, debemos encontrar el valor t_{0.025} que satisface P(T > t_{0.025}) = 0.025.
+
+(t.025 <- qt(p = 0.025, df= 41, lower.tail = FALSE))
+# Como
+
+(t0 < -t.025) | (t0 > t.025)
+# rechazamos la hipótesis nula.
+
+# p-value El p-value lo podemos calcular como
+
+(pvalue <- 2*pt(t0, df = 41, lower.tail = FALSE))
+
+### También podemos usar la función t.test para llevar a cabo el procedimiento 
+# de contraste de hipótesis
+
+t.test(x = m1, y = m2,
+       alternative = "two.sided",
+       mu = 0, paired = FALSE, var.equal = TRUE)
 
