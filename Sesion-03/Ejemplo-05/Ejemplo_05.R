c91b16ab6f883c8b80296b9e9adcc27c961db303
Erick <e.coronadoj@gmail.com>
2021-06-22 22:17:42 -0500

    Update Ejemplo_05.R

--- Sesion-03/Ejemplo-05/Ejemplo_05.R
+++ Sesion-03/Ejemplo-05/Ejemplo_05.R
@@ -1,11 +1,12 @@
+# Ejemplo 5. Sesión 3
 # Al inicio es posible que no comprendas todo el código, trata de leerlo e ir asimilando que es lo que realiza cada línea. 
 
 # library(ggplot2)
 # install.packages("scales")
 library(scales) # Para mejorar la lectura de las etiquetas  en el eje de las x de tipo fecha
-# Ahora vamos a leer nuestro archivo C19Mexico.csv con los infectados y muertos acumulados para cada fecha
+# Ahora vamos a leer nuestro archivo C19Mexico.csv con los infectados y Decesos acumulados para cada fecha
 
-mex <- read.csv("C19Mexico.csv")
+mex <- read.csv("C19Mexico.csv") # Carga tu fichero
 
 head(mex); tail(mex)
 
@@ -69,9 +70,9 @@ p
 
 # Muertes Acumuladas
 
-mexm <- subset(mex, Muertos > 0) # Tomamos el subconjunto desde que comenzaron las muertes
+mexm <- subset(mex, Decesos > 0) # Tomamos el subconjunto desde que comenzaron las muertes
 
-p <- ggplot(mexm, aes(x=Fecha, y=Muertos)) + geom_line( color="red") + 
+p <- ggplot(mexm, aes(x=Fecha, y=Decesos)) + geom_line( color="red") + 
   geom_point() +
   labs(x = "Fecha", 
        y = "Muertes acumuladas",
@@ -88,11 +89,11 @@ p
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
   annotate("text", x = mexm$Fecha[round(dim(mexm)[1]*0.4)], 
-           y = max(mexm$Muertos), colour = "red", size = 5, label = paste("Última actualización: ", mexm$Muertos[dim(mexm)[1]]))
+           y = max(mexm$Decesos), colour = "red", size = 5, label = paste("Última actualización: ", mexm$Decesos[dim(mexm)[1]]))
 p
 
 # Muertes por Día
-p <- ggplot(mexm, aes(x=Fecha, y=NM)) + 
+p <- ggplot(mexm, aes(x=Fecha, y=ND)) + 
   geom_line(stat = "identity") + 
   labs(x = "Fecha", y = "Número de nuevos decesos",
        title = paste("Nuevos decesos por COVID-19 en México:", 
@@ -103,12 +104,12 @@ p <- ggplot(mexm, aes(x=Fecha, y=NM)) +
 
 p <- p  + scale_x_date(labels = date_format("%d-%m-%Y"))
 
-###
+### 
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
   annotate("text", x = mexm$Fecha[round(dim(mexm)[1]*0.2)], 
-           y = max(mexm$NM), colour = "red", size = 5, label = paste("Última actualización: ", mexm$NM[dim(mexm)[1]]))
+           y = max(mexm$ND), colour = "red", size = 5, label = paste("Última actualización: ", mexm$ND[dim(mexm)[1]]))
 p
 
 # Acumulado de Casos Confirmados y Muertes
@@ -116,7 +117,7 @@ p <- ggplot(mex, aes(x=Fecha, y=Infectados)) + geom_line(color="blue") +
   labs(x = "Fecha", 
        y = "Acumulado de casos",
        title = paste("COVID-19 en México:", format(Sys.time(), tz="America/Mexico_City",usetz=TRUE))) +
-  geom_line(aes(y = Muertos), color = "red") +
+  geom_line(aes(y = Decesos), color = "red") +
   theme(axis.text.x = element_text(face = "bold", color="#993333" , size = 10, angle = 45, hjust = 1),
         axis.text.y = element_text(face = "bold", color="#993333" , size = 10, angle = 45, hjust = 1))  # color, Ángulo y estilo de las abcisas y ordenadas
 
@@ -129,7 +130,7 @@ p <- p +
   annotate("text", x = mex$Fecha[round(dim(mex)[1]*0.4)], 
            y = max(mex$Infectados), colour = "blue", size = 5, label = paste("Última actualización para Infectados:", mex$Infectados[dim(mex)[1]])) +
   annotate("text", x = mex$Fecha[round(dim(mex)[1]*0.4)], 
-           y = max(mex$Infectados)-100000, colour = "red", size = 5, label = paste("Última actualización para Muertes:", mex$Muertos[dim(mex)[1]])) 
+           y = max(mex$Infectados)-100000, colour = "red", size = 5, label = paste("Última actualización para Muertes:", mex$Decesos[dim(mex)[1]])) 
 p
 
 # Tasa de Letalidad: La tasa de letalidad observada para un día determinado, la calculamos dividiendo las muertes acumuladas reportadas hasta ese día, entre el acumulado de casos confirmados para el mismo día. Multiplicamos el resultado por 100 para reportarlo en forma de porcentaje. Lo que obtenemos es el porcentaje de muertes del total de casos confirmados.
@@ -157,13 +158,13 @@ p
 
 # El factor de crecimiento de infectados para un día determinado, lo calculamos al dividir el acumulado de infectados para ese día, entre el acumulado de infectados del día anterior. El factor de crecimiento de muertes lo calculamos de forma similar.
 
-mex <- filter(mex, FCM < Inf) # Tomamos solo valores reales de factores de crecimiento
+mex <- filter(mex, FCD < Inf) # Tomamos solo valores reales de factores de crecimiento
 
 p <- ggplot(mex, aes(x=Fecha, y=FCI)) + geom_line(color="blue") + 
   labs(x = "Fecha", 
        y = "Factor de crecimiento",
        title = paste("COVID-19 en México:", format(Sys.time(), tz="America/Mexico_City",usetz=TRUE))) +
-  geom_line(aes(y = FCM), color = "red") + theme(plot.title = element_text(size=12)) +
+  geom_line(aes(y = FCD), color = "red") + theme(plot.title = element_text(size=12)) +
   theme(axis.text.x = element_text(face = "bold", color="#993333" , size = 10, angle = 45, hjust = 1),
         axis.text.y = element_text(face = "bold", color="#993333" , size = 10, angle = 45, hjust = 1))  # color, Ángulo y estilo de las abcisas y ordenadas
 
@@ -172,6 +173,6 @@ p <- p  + scale_x_date(labels = date_format("%d-%m-%Y"))
 ###
 
 p <- p +
-  annotate("text", x = mex$Fecha[round(length(mex$Fecha)*0.4)], y = max(mex$FCM), colour = "blue", size = 5, label = paste("Última actualización para infectados: ", round(mex$FCI[dim(mex)[1]], 4))) +
-  annotate("text", x = mex$Fecha[round(length(mex$Fecha)*0.4)], y = max(mex$FCM)-0.2, colour = "red", size = 5, label = paste("Última actualización para muertes: ", round(mex$FCM[dim(mex)[1]], 4))) 
+  annotate("text", x = mex$Fecha[round(length(mex$Fecha)*0.4)], y = max(mex$FCD), colour = "blue", size = 5, label = paste("Última actualización para infectados: ", round(mex$FCI[dim(mex)[1]], 4))) +
+  annotate("text", x = mex$Fecha[round(length(mex$Fecha)*0.4)], y = max(mex$FCD)-0.2, colour = "red", size = 5, label = paste("Última actualización para muertes: ", round(mex$FCD[dim(mex)[1]], 4))) 
 p
